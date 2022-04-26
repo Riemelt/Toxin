a71a3adaa4cdb6d0e80ff6787490c1b47a0f1b00
Riemelt <n.lychenkov@gmail.com>
2022-04-26 22:27:09 +0700

    Finish dropdown, add counter-item component

--- src/components/dropdown-counter/dropdown-counter.js
+++ src/components/dropdown-counter/dropdown-counter.js
@@ -1,36 +1,36 @@
 import InputField from "../input-field/input-field.js";
 import ControlPanel from "../control-panel/control-panel.js";
+import CounterItem from "../counter-item/counter-item.js";
+
+import { declOfNum } from "../../scripts/functions.js";
 
 class DropdownCounter {
   #className = "dropdown-counter";
 
-  #increment = `js-${this.#className}__increment`;
-  #decrement = `js-${this.#className}__decrement`;
-  #counterValue = `js-${this.#className}__value`;
-
   #$component;
   #$menu;
-  #$decrements;
-  #$increments;
-  #$counterValues;
+  #$counterItems;
 
   #inputField;
   #controlPanel;
   #dropdownType;
+  #counterItems = [];
   #itemList;
 
-  static #MIN = 0;
-  static #MAX = 999;
+  static #PLACEHOLDERS = {
+    guest: "Сколько гостей",
+    room: "Сколько комнат",
+  }
 
   static #DICTIONARY = {
     guest: {
-      guest: "гость",
-      baby: "младенец"
+      guest: ["гость", "гостя", "гостей"],
+      baby: ["младенец", "младенца", "младенцев"],
     },
     room: {
-      room: "спальня",
-      bathroom: "ванная комната",
-      bed: "кровать"
+      room: ["спальня", "спальни", "спален"],
+      bathroom: ["ванная комната", "ванные комнаты", "ванных комнат"],
+      bed: ["кровать", "кровати", "кроватей"],
     },
   }
 
@@ -43,13 +43,11 @@ class DropdownCounter {
     this.#$component = $parent.find(`.js-${this.#className}`);
 
     this.#$menu = this.#$component.find(`.js-${this.#className}__menu`);
-    this.#$decrements = this.#$component.find(`.js-${this.#className}__decrement`);
-    this.#$increments = this.#$component.find(`.js-${this.#className}__increment`);
-    this.#$counterValues = this.#$component.find(`.js-${this.#className}__value`);
+    this.#$counterItems = this.#$component.find(`.js-${this.#className}__item`);
+    this.#$counterItems.each(this.#initCounterItem.bind(this));
 
     this.#dropdownType = this.#$component.data("type");
     this.#itemList = this.#initItemList(this.#dropdownType);
-
     this.#inputField = new InputField(this.#$component);
     this.#controlPanel = new ControlPanel(this.#$component, {
       handleButtonClick: this.#handleControlPanelClick.bind(this)
@@ -60,8 +58,6 @@ class DropdownCounter {
     jQuery(this.#handleInputInit.bind(this));
     $(document).on("click.dropdown", this.#handleDocumentClick.bind(this));
     this.#$component.on("click.dropdown", this.#handleDropdownClick.bind(this));
-    this.#$increments.on("click.dropdown", this.#handleIncrementClick.bind(this));
-    this.#$decrements.on("click.dropdown", this.#handleDecrementClick.bind(this));
   }
 
   #render() {
@@ -84,39 +80,14 @@ class DropdownCounter {
       this.#toggleDropdown();
   }
 
-  #handleIncrementClick(e) {
-    const $increment = $(e.currentTarget);
-    const $counterValue = $increment.parent().find(`.${this.#counterValue}`);
-    const $decrement = $increment.parent().find(`.${this.#decrement}`);
-  
-    const value = $counterValue.html();
-    const newValue = Number(value) + 1;
-
-    this.#updateCounterItem({ $decrement, $counterValue, $increment, newValue });
-  }
-
-  #handleDecrementClick(e) {
-    const $decrement = $(e.currentTarget);
-    const $counterValue = $decrement.parent().find(`.${this.#counterValue}`);
-    const $increment = $decrement.parent().find(`.${this.#increment}`);
-  
-    const value = $counterValue.html();
-    const newValue = Number(value) - 1;
-
-    this.#updateCounterItem({ $decrement, $counterValue, $increment, newValue });
+  #handleCounterItemClick(addedValue, type) {
+    this.#itemList[type] += addedValue;
+    this.#updateInput();
   }
 
   #handleDocumentClick(e) {
     if (!this.#isDropdown(e.target))
       this.#closeDropdown();
-  }
-
-  #isMenu(target) {
-    return this.#$menu.is(target) || this.#$menu.has(target).length !== 0;
-  }
-
-  #isDropdown(target) {
-    return this.#$component.is(target) || this.#$component.has(target).length !== 0;
   }
   
   #handleControlPanelClick(type) {
@@ -130,43 +101,55 @@ class DropdownCounter {
   }
 
   #handleApplyButtonClick() {
-    console.log("Apply button has been clicked!");
+    this.#closeDropdown();
   }
 
   #handleResetButtonClick() {
-    console.log("Reset button has been clicked!");
+    this.#resetCounterItems();
   }
 
   #handleInputInit() {
-    this.#$counterValues.each(this.#renderCounterValue.bind(this));
+    this.#counterItems.forEach(this.#renderCounterValue.bind(this));
     const text = this.#buildString(this.#itemList, this.#dropdownType);
     this.#inputField.setInputText(text);
   }
 
-  #renderCounterValue(_, element) {
-    const $value = $(element);
-    const type = $value.data("type");
-    const guestCount = $value.html();
-    this.#itemList[type] += Number(guestCount);
+  #updateInput() {
+    const text = this.#buildString(this.#itemList, this.#dropdownType);
+    this.#inputField.setInputText(text);
   }
 
-  #updateCounterItem(item) {
-    const { $decrement, $counterValue, $increment } = item;
-    let { newValue } = item;
+  #resetCounterItems() {
+    this.#counterItems.forEach(this.#resetCounterValue.bind(this));
+    this.#itemList = this.#initItemList(this.#dropdownType);
+    this.#updateInput();
+  }
 
-    if (newValue <= DropdownCounter.#MIN) {
-      newValue = DropdownCounter.#MIN;
-      $decrement.prop("disabled", true);
-    }
-    else if (newValue >= DropdownCounter.#MAX) {
-      newValue = DropdownCounter.#MAX;
-      $increment.prop("disabled", true);
-    }
-    else {
-      $decrement.prop("disabled", false);
-      $increment.prop("disabled", false);
-    }
-    $counterValue.html(newValue);
+  #resetCounterValue(element) {
+    element.setValue(0);
+  }
+
+  #renderCounterValue(element) {
+    const value = element.getValue();
+    const type = element.getType();
+    this.#itemList[type] += value;
+  }
+
+  #initCounterItem(_, element) {
+    const $counterItem = $(element);
+    this.#counterItems.push(
+      new CounterItem($counterItem, {
+        handleCounterItemClick: this.#handleCounterItemClick.bind(this),
+      })
+    );
+  }
+
+  #isMenu(target) {
+    return this.#$menu.is(target) || this.#$menu.has(target).length !== 0;
+  }
+
+  #isDropdown(target) {
+    return this.#$component.is(target) || this.#$component.has(target).length !== 0;
   }
 
   #buildString(itemList, type) {
@@ -174,12 +157,16 @@ class DropdownCounter {
     const dictionaryMap = DropdownCounter.#DICTIONARY[type];
   
     Object.keys(itemList).forEach(function(item) {
-      if (itemList[item] > 0) {
+      const itemCount = itemList[item];
+      if (itemCount > 0) {
         if (text !== "")
           text += ", ";
-        text += itemList[item] + " " + dictionaryMap[item];
+        text += itemCount + " " + declOfNum(itemCount, dictionaryMap[item]);
       }
     });
+
+    if (text === "")
+      text = DropdownCounter.#PLACEHOLDERS[type];
     return text;
   }
 
