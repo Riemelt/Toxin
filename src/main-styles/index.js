9caf22f0689917bea4110dbca57fd1dc2040363d
Riemelt <n.lychenkov@gmail.com>
2022-07-14 08:42:23 +0700

    Install font awesome, add socials component

--- src/main-styles/index.js
+++ src/main-styles/index.js
@@ -1,3 +1,5 @@
 import "./fonts.scss";
 import "./base.scss";
 import "./material-icons.scss";
+import "@fortawesome/fontawesome-free/scss/fontawesome.scss";
+import "@fortawesome/fontawesome-free/scss/brands.scss";
